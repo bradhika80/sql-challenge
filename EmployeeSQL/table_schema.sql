f40c3f232e8cd2549f211c8402bd2ce2efdd20e1
bradhika80 <bradhika80@gmail.com>
2020-10-07 18:00:16 -0500

    build table schema

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -25,9 +25,26 @@ Create Table Department (
   Dept_No varchar(10) PRIMARY KEY,
   Dept_Name varchar(50) NOT NULL);
   
+Create Table Dept_Emp (
+	Emp_No int NOT NULL,
+	Dept_No varchar(10) NOT NULL,
+	Primary Key (Emp_No, Dept_No));
 
+-- Add foreign key to Dept_Emp for Emp_No. 
+-- If employee is deleted then the record is deleted. If emp_no is updated, the updates are cascaded in the Dept_Emp
+Alter Table only Dept_Emp Add CONSTRAINT Emp_No_fkey FOREIGN KEY (Emp_No) REFERENCES Employee(Emp_No) 
+                       ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- Add foreign key to Dept_Emp for Dept_No. 
+--If department is deleted with employees in department, it is restricted. If no records for corresponding dept in Dept_Emp it is deleted.
+-- if dept_no is updated the changes are cascaded to the employees in the dept.
+Alter Table only Dept_Emp Add CONSTRAINT Dept_No_fkey FOREIGN KEY (Dept_No) REFERENCES Employee(Emp_No) 
+                       ON UPDATE CASCADE ON DELETE RESTRICT;
+					   
+					   
 -- Add foreign key to employee table with Title 
 ALTER TABLE ONLY Employee
-    ADD CONSTRAINT emp_title_id_fkey FOREIGN KEY (Emp_Title_Id) REFERENCES title(title_id);
+    ADD CONSTRAINT emp_title_id_fkey FOREIGN KEY (Emp_Title_Id) REFERENCES title(title_id) ON UPDATE CASCADE ON DELETE RESTRICT;
+;
 
 
