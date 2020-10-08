80c1ad7abde33bf8298208229144dbc4c6b21f9e
bradhika80 <bradhika80@gmail.com>
2020-10-07 22:02:53 -0500

    list employee with year 1986

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -7,4 +7,11 @@ select
 	  e.sex as "Sex", 
 	  s.salary  as "Salary"
 	  from employee e join salary s on s.emp_no = e.emp_no;
-	  
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+
+select 
+	  last_name as "Last Name",
+	  first_name as "First Name",
+	  hire_date as "Hire Date" 
+	  from employee where date_part('year', hire_date) = 1986
